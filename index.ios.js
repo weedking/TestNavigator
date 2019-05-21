124a6f6bbd0a5238ce5d697cdb6de91a6553441b
weedking <260815998@qq.com>
2019-05-21 11:47:57 +0800

    可获得公司列表，点击公司名，进入详情页。

--- index.ios.js
+++ index.ios.js
@@ -5,81 +5,82 @@
  */
 
 import React, { Component } from 'react';
-import {AppRegistry, StyleSheet, FlatList, Text, TextInput, ListView, View, Image, TouchableOpacity, TouchableHighlight,ScrollView} from 'react-native';
-import { Button, List } from 'antd-mobile-rn';
+import {AppRegistry, StyleSheet, FlatList, Text, TextInput, ListView, View, Image, TouchableOpacity,
+    TouchableHighlight,ScrollView, Dimensions, ActivityIndicator, RefreshControl} from 'react-native';
+import { Button, List, SearchBar } from 'antd-mobile-rn';
 import {Navigator } from 'react-native-deprecated-custom-components';
 
 const Item = List.Item;
 const Brief = Item.Brief;
 
 export default class TestNavigator extends Component {
-  configureScene(route, routeStack)
-  {
-    if (route.type == 'Modal') {
-      return Navigator.SceneConfigs.FloatFromBottom;
+    configureScene(route, routeStack)
+    {
+        if (route.type == 'Modal') {
+            return Navigator.SceneConfigs.FloatFromBottom;
+        }
+        return Navigator.SceneConfigs.PushFromRight;
     }
-    return Navigator.SceneConfigs.PushFromRight;
-  }
 
-  render() {
-    return (
-        <Navigator
-            style={{flex:1}}
-            initialRoute={{component: FirstPage, passProps: {title: '首页', rightText: '菜单'}}}
-            configureScene={this.configureScene}
-            renderScene={(route, navigator) => <route.component route={route} navigator={navigator} {...route.passProps} />}
-            navigationBar={
-              <Navigator.NavigationBar
-                  style={styles.navContainer}
-                  routeMapper={NavigationBarRouteMapper}
-              />}
-        />
-    );
-  }
+    render() {
+        return (
+            <Navigator
+                style={{flex:1}}
+                initialRoute={{component: FirstPage, passProps: {title: '首页', rightText: '菜单'}}}
+                configureScene={this.configureScene}
+                renderScene={(route, navigator) => <route.component route={route} navigator={navigator} {...route.passProps} />}
+                navigationBar={
+                    <Navigator.NavigationBar
+                        style={styles.navContainer}
+                        routeMapper={NavigationBarRouteMapper}
+                    />}
+            />
+        );
+    }
 }
 
 // 首页
 class FirstPage extends Component
 {
-  /**
-   * 跳转
-   */
-  gotoPage(component, title)
-  {
-    this.props.navigator.push(
-        {
-          component: component,
-          passProps: {
-            // title: '二级页面',
-              title: title,
-            lastPageTitle: this.props.title
+    /**
+     * 跳转
+     */
+    gotoPage(component, title)
+    {
+        this.props.navigator.push(
+            {
+                component: component,
+                passProps: {
+                    // title: '二级页面',
+                    title: title,
+                    lastPageTitle: this.props.title
 
-          }
-        })
-  }
+                }
+            })
+    }
 
-  render()
-  {
-    return (
-        <View style={{paddingTop: 80}}>
+    render()
+    {
+        return (
+            <View style={{paddingTop: 80}}>
 
-          {/*<Button onClick={() => this.gotoPage(SecondPage, '二级页面')}>*/}
-            {/*<Text style={{fontSize:28, padding: 12}}>点击跳转到二级页面</Text>*/}
-            {/*/!*<Text style={{padding: 10, fontSize: 20}}>这是首页，这是首页，这是首页，这是首页，这是首页</Text>*!/*/}
-          {/*</Button>*/}
+                {/*<Button onClick={() => this.gotoPage(SecondPage, '二级页面')}>*/}
+                {/*<Text style={{fontSize:28, padding: 12}}>点击跳转到二级页面</Text>*/}
+                {/*/!*<Text style={{padding: 10, fontSize: 20}}>这是首页，这是首页，这是首页，这是首页，这是首页</Text>*!/*/}
+                {/*</Button>*/}
 
-         <TouchableOpacity onPress={()=>{this.gotoPage(SecondPage, '二级页面');}}>
-            <Image source={require('./img/customer64.png')} />
-         </TouchableOpacity>
-        </View>
-    );
-  }
+                <TouchableOpacity onPress={()=>{this.gotoPage(SecondPage, '二级页面');}}>
+                    <Image source={require('./img/customer64.png')} />
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 // 二级页面
 class SecondPage extends Component
 {
-     // test1=111;
+    // test1=111;
     constructor(props){
         super(props);
         this.state = {
@@ -88,6 +89,7 @@ class SecondPage extends Component
             b:2,
             id:9,
             c:99,
+            data:[],
             dataSource2:[
                 {
                     "id": 1,
@@ -105,22 +107,22 @@ class SecondPage extends Component
                     "remark": "初步接触",
                     "inputtime": null
                 },
-        {
-            "id": 70,
-            "key1": "请填写key",
-            "name": "请填写公司名称",
-            "city": "请填写城市",
-            "phone": "请填写电话",
-            "contacts": "请填写联系人",
-            "email": "请填写邮箱",
-            "source": "请填写来源",
-            "title": "请填写职务",
-            "need": "请填写产品kk",
-            "url": "请填写网址",
-            "address": "请填写公司地址",
-            "remark": "请填写备注",
-            "inputtime": null
-        }]
+                {
+                    "id": 70,
+                    "key1": "请填写key",
+                    "name": "请填写公司名称",
+                    "city": "请填写城市",
+                    "phone": "请填写电话",
+                    "contacts": "请填写联系人",
+                    "email": "请填写邮箱",
+                    "source": "请填写来源",
+                    "title": "请填写职务",
+                    "need": "请填写产品kk",
+                    "url": "请填写网址",
+                    "address": "请填写公司地址",
+                    "remark": "请填写备注",
+                    "inputtime": null
+                }]
 
         };
     }
@@ -202,7 +204,7 @@ class SecondPage extends Component
         // const {id} = this.state.dataSource[0].id
         this.setState({ a: a-1,}
             // dataSource:dataSource+''}
-            );
+        );
         alert('a减去1后的值：' + a);
     }
 
@@ -213,32 +215,62 @@ class SecondPage extends Component
         alert('a加上1后的值：' + a);
     }
 
+    //List的分割线
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: "86%",
+                    backgroundColor: "#CED0CE",
+                    marginLeft: "14%"
+                }}
+            />
+        );
+    };
 
-  render()
-  {
+    // Header
+    renderHeader = () => {
+        return <SearchBar placeholder="Type Here..." lightTheme round />;
+    };
 
-    return (
+    // Footer
+    renderFooter = () => {
+        return <Text style={[styles.txt,{backgroundColor:'black'}]}>这是尾部</Text>;
+
+    };
+
+    _keyExtractor = (item, index) => item.id + '_' + index;
+
+    render()
+    {
+
+        // const { params } = this.props.navigation.state;
+        return (
+            <View style={{flex: 1, paddingTop:100}}>
+                <FlatList
+                    data={this.state.dataSource}
+                    renderItem={({item}) => <TouchableOpacity style={styles.flatitemview} onPress={()=>{this.gotoPage1(HuiYuan, <Text> {item.name}</Text>);}}>
+                        <Text> {item.name}</Text>
+                        </TouchableOpacity>
+                    }
+
+                    // keyExtractor={({id}, index) => id}
+                    keyExtractor={this._keyExtractor} //把item.id作为key
+                />
+            </View>
 
 
 
-                            <View style={{flex: 1, paddingTop:100}}>
-                                <FlatList
-                                    data={this.state.dataSource}
-                                    renderItem={({item}) => <Text>{item.title}, {item.name}</Text>}
-                                    keyExtractor={({id}, index) => id}
-                                />
-                            </View>
+
+        );
+    }
 
 
-      // <div>
-      //
-      //     {this.state.dataSource.map(
-      //         //传递点中的ID
-      //         u => <div key={u.id} >{u.name}</div>)
-      //     }
-      // </div>
-    );
-  }
+
+
+
+
 }
 
 
@@ -260,8 +292,8 @@ class HuiYuan extends Component{
 // 导航栏的Mapper
 var NavigationBarRouteMapper =
     {
-      // 左键
-      LeftButton: (route, navigator, index, navState) =>
+        // 左键
+        LeftButton: (route, navigator, index, navState) =>
         {
             if (index <= 0) {
                 return null;
@@ -273,91 +305,113 @@ var NavigationBarRouteMapper =
                 );
             }
         },
-      // 右键
-      RightButton(route, navigator, index, navState)
-      {
-        // if(!route.passProps.rightText) return null;
-        // return (
-        //     <View style={{paddingTop: 80}}>
-        //
-        //       <Button onClick={() => alert('测试菜单')}>
-        //         {route.passProps.rightText}
-        //       </Button>
-        //
-        //     </View>
-        //
-        // );
-          return null;
-      },
-      // 标题
-      Title(route, navigator, index, navState)
-      {
-        return (
-            <Text style={styles.title}>
-              {route.passProps.title || '默认标题'}
-            </Text>
-        );
-      }
+        // 右键
+        RightButton(route, navigator, index, navState)
+        {
+            // if(!route.passProps.rightText) return null;
+            // return (
+            //     <View style={{paddingTop: 80}}>
+            //
+            //       <Button onClick={() => alert('测试菜单')}>
+            //         {route.passProps.rightText}
+            //       </Button>
+            //
+            //     </View>
+            //
+            // );
+            return null;
+        },
+        // 标题
+        Title(route, navigator, index, navState)
+        {
+            return (
+                <Text style={styles.title}>
+                    {route.passProps.title || '默认标题'}
+                </Text>
+            );
+        }
     };
 
 const styles = StyleSheet.create({
-  // 页面框架
-  container: {
-    flex: 4,
-    marginTop: 100,
-    flexDirection: 'column'
-  },
-  // 导航栏
-  navContainer: {
-    backgroundColor: '#41ABF7',
-    paddingTop: 12,
-    paddingBottom: 10,
-    flex: 1
-  },
-  // 导航栏文字
-  headText: {
-    color: '#ffffff',
-    fontSize: 22
-  },
-  // 按钮
-  button: {
-    height: 120,
-    marginTop: 10,
-    justifyContent: 'center', // 内容居中显示
-    backgroundColor: '#ff1049',
-    alignItems: 'center'
-  },
-  // 按钮文字
-  buttonText: {
-    fontSize: 18,
-    color: '#ffffff'
-  },
-  // 左面导航按钮
-  leftNavButtonText: {
-    color: '#ffffff',
-    fontSize: 18,
-    marginLeft: 13,
-    marginTop: 12,
-    flex: 1
-  },
-  // 右面导航按钮
-  rightNavButtonText: {
-    color: '#ffffff',
-    fontSize: 18,
-    marginRight: 13,
-    marginTop: 12,
-    flex: 1
-  },
-  // 标题
-  title: {
-    fontSize: 18,
-    color: '#fff',
-    textAlign: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontWeight: 'bold',
-    marginTop: 12
-  }
+    // 页面框架
+    container: {
+        flex: 4,
+        marginTop: 100,
+        flexDirection: 'column'
+    },
+    // 导航栏
+    navContainer: {
+        backgroundColor: '#41ABF7',
+        paddingTop: 12,
+        paddingBottom: 10,
+        flex: 1
+    },
+    // 导航栏文字
+    headText: {
+        color: '#ffffff',
+        fontSize: 22
+    },
+    // 按钮
+    button: {
+        height: 120,
+        marginTop: 10,
+        justifyContent: 'center', // 内容居中显示
+        backgroundColor: '#ff1049',
+        alignItems: 'center'
+    },
+    // 按钮文字
+    buttonText: {
+        fontSize: 18,
+        color: '#ffffff'
+    },
+    // 左面导航按钮
+    leftNavButtonText: {
+        color: '#ffffff',
+        fontSize: 18,
+        marginLeft: 13,
+        marginTop: 12,
+        flex: 1
+    },
+    // 右面导航按钮
+    rightNavButtonText: {
+        color: '#ffffff',
+        fontSize: 18,
+        marginRight: 13,
+        marginTop: 12,
+        flex: 1
+    },
+    // 标题
+    title: {
+        fontSize: 18,
+        color: '#fff',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontWeight: 'bold',
+        marginTop: 12
+    },
+    txt: {
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        color: 'white',
+        fontSize: 30,
+    },
+    flatitemimg: {
+     width: '100%',
+         height: 200,
+    },
+    flatitemview: {
+    flex: 1,
+        marginRight:10,
+        marginBottom:10,
+    },
+    flatlistview: {
+    // width: deviceWidth,
+        paddingLeft:10,
+        paddingTop:23,
+        marginBottom:250,
+}
+
 });
 
 AppRegistry.registerComponent('TestNavigator', () => TestNavigator);
